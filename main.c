0a88fa9134479725d85c2e1707edbee746bcf9d6
guizamberlam <guizamberlam@hotmail.com>
2018-10-30 22:16:41 -0300

    Inicio da construção do Indice Primario

--- main.c
+++ main.c
@@ -3,6 +3,13 @@
 #include <string.h>
 #include <locale.h>
 
+typedef struct{
+	int byte_o;
+	int id;
+}indice;
+
+indice indice_primario[55];
+
 int getline (char *str, FILE *arq){
 	int i = 0;
 	fgets(str, 20, arq);
@@ -15,19 +22,28 @@ int getline (char *str, FILE *arq){
 // Função para ler um arquivo e o formatar para um arquivo com o tamanho antes de cada registro com os campos separados por pipes
 void importar(FILE *arq){
 	int i;
-	char buffer[100];
-	buffer[0] = '\0';
-	char string[20];
-	FILE *destino = fopen("destino.txt", "w");
+	int byte_offset = 0, aux = 0;
+	char buffer[100];								// buffer para armazenar os dados do registro
+	buffer[0] = '\0';								// inicia ele vazio	
+	char string[20];								// auxiliar para ler uma linha do arquivo inicial
+	FILE *destino = fopen("destino.txt", "w");		// arquivo onde será escrito os registros
 	while( !feof(arq) ){
 		for(i = 0; i < 4; i++){
-			getline(string, arq);
-			strcat(buffer, string);
+			getline(string, arq);	// le uma linha do arquivo
+			
+			if(i == 0){			// pega o byte offset e os valores para o indice primario
+				indice_primario[aux].id = atoi(string);
+				indice_primario[aux].byte_o = byte_offset;
+				aux ++;
+			}
+			strcat(buffer, string);			// concatena o lido com um buffer para armazenar o registro inteiro
 		}
-		i = strlen(buffer);
-		fwrite(&i, sizeof(int), 1, destino);
-		fwrite(buffer, sizeof(char), i, destino);
-		buffer[0] = '\0';
+		i = strlen(buffer);		// calcula o tamanho do registro
+		byte_offset = byte_offset + i + 4;		// calcula o byte offset do proximo registro
+		fwrite(&i, sizeof(int), 1, destino);		// escreve o tamanho do registro 
+		fwrite(buffer, sizeof(char), i, destino);		// escreve o registro
+		buffer[0] = '\0';							// limpa o buffer
+									
 	}
 	fclose(destino);
 }
@@ -52,21 +68,28 @@ void menu(){
 				}else{
 					importar(individuos);
 				}
+				break;
 			
 			case 2:
 				// buscar cão
+				break;
 				
 			case 3:
 				// buscar por raça
+				break;
 			
 			default:
 				printf("Encerrando o programa!");
 		}
-		system("cls");
+		//system("cls");
 	}
 }
 
 int main(){
 	setlocale(LC_ALL, "Portuguese");
 	menu();
+	int i;
+	for(i = 0; i<55; i++){
+		printf("ID: %d \t Byte Offset: %d\n", indice_primario[i].id, indice_primario[i].byte_o);
+	}
 }
